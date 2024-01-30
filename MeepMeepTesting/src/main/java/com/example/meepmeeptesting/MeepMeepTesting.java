7ca0c8ad64556dfaafbb235a4aab76192f2011b9
matei <mateibalan26@gmail.com>
2024-01-30 17:56:51 +0200

    Includere modificari

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,7 +1,7 @@
 package com.example.meepmeeptesting;
 
-import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -12,40 +12,28 @@ public class MeepMeepTesting {
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 12.6)
-                .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(53, 36, 0))
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
 
-                                .strafeTo(new Vector2d(-35, -10))
-                                .splineToSplineHeading(new Pose2d(-60, -60, -Math.PI * 3 / 4), -Math.PI * 3 / 4)
+        myBot.runAction(myBot.getDrive().actionBuilder(new  Pose2d(-38, 61, -Math.PI / 2))
+                .strafeTo(new Vector2d(-49,32))
+                .strafeTo(new Vector2d(-49,36))
+                .strafeToLinearHeading(new Vector2d(48,36),0)
+                .strafeTo(new Vector2d(53,29))
+                .strafeTo(new Vector2d(11,11))
+                .strafeTo(new Vector2d(-58,11))
+//                .strafeTo(new Vector2d(-58,23.5))
+                .strafeTo(new Vector2d(-58,35.5))
+                .waitSeconds(1)
+                .splineTo(new Vector2d(-40,11),0)
+//                .lineToY(11)
+//                .lineToX(-58)
+//                .lineToY(20)
+                .build());
 
-                                .strafeTo(new Vector2d(-40, -20))
-
-//                                .splineToSplineHeading(new Pose2d(10, 20, 0), 0)
 
 
 
-//                                .strafeTo(new Vector2d(-30, -10))
-//                                .lineTo(new Vector2d(0,0))
-                                .splineToSplineHeading(new Pose2d(0, 0, 0), 0)
-                                .splineToConstantHeading(new Vector2d(53, 36), 0)
-//                                .strafeTo(new Vector2d(53, 36))
-//                                .splineToConstantHeading(new Vector2d(53, 36), 0)
-
-
-//                                .strafeTo(new Vector2d(-49,36))
-
-//                                .strafeTo(new Vector2d(53,33))
-//                                .lineTo(new Vector2d(-41,58))
-//                                .strafeTo(new Vector2d(-43,34) )
-//                                .lineToLinearHeading(new Pose2d(48, 36, 0))
-//                                .strafeTo(new Vector2d(53,31))
-//                                .strafeTo(new Vector2d(10,11))
-//                                .lineTo(new Vector2d(-50,11))
-//                                .splineTo(new Vector2d(10, 10), 0)
-
-                                                .build()
-                                );
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
                 .setDarkMode(true)
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Camera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Camera.java
@@ -1,15 +1,195 @@
 package org.firstinspires.ftc.teamcode.OpModes;
 
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
-import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.OpenCv.OpenCvPipRosu;
+import org.firstinspires.ftc.teamcode.RR.MecanumDrive;
+import org.firstinspires.ftc.teamcode.subsystems.Lift;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
 
+@TeleOp
 public class Camera extends LinearOpMode {
 
+    private static final int CAMERA_WIDTH = 1280;
+    private static final int CAMERA_HEIGHT = 720;
 
+
+    private OpenCvCamera controlHubCam;  // Use OpenCvCamera class from FTC SDK
+    private static volatile OpenCvPipRosu.detectie nou;
+
+    Action pixelToBoardNT,boardToMij,exactBoard;
     @Override
-    public void runOpMode(){
+    public void runOpMode() throws InterruptedException {
+
+        //albastru dep
+        Pose2d beginPose = new Pose2d(13, 61, -Math.PI / 2);
+        Pose2d almostBoard = new Pose2d(48,36,0);
+        Pose2d boardMij = new Pose2d(53,36,0);
+        Pose2d boardSt = new Pose2d(53,41,0);
+        Pose2d boardDr = new Pose2d(53,29,0);
+        Pose2d mij = new Pose2d(11,11,0);
+        Pose2d stackFront = new Pose2d(-58,11,0);
+        Pose2d stackMij = new Pose2d(-58,23.5,0);
+        Pose2d stackLast = new Pose2d(-58,35.5,0);
+        Pose2d stackPreg = new Pose2d(-40,11,0);
+
+
+
+        Lift lift = new Lift();
+        DcMotor leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
+        DcMotor leftRear = hardwareMap.get(DcMotorEx.class, "leftRear");
+        DcMotor rightRear = hardwareMap.get(DcMotorEx.class, "rightRear");
+        DcMotor rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+
+
+
+        initOpenCV(telemetry);
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
+        FtcDashboard.getInstance().startCameraStream(controlHubCam, 30);
+        int v[] = new int[4];
+        v[1]=0;
+        v[2]=0;
+        v[3]=0;
+        while(opModeInInit() && !isStopRequested()){
+            nou = OpenCvPipRosu.getLocugasit();
+            if(nou==OpenCvPipRosu.detectie.Dreapta)v[1]++;
+            else if(nou==OpenCvPipRosu.detectie.Stanga)v[2]++;
+            else v[3]++;
+            telemetry.addData("Detect",nou );
+            telemetry.update();
+        }
+        controlHubCam.stopStreaming();
+
+
+
+
+        MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
+
+        if(v[1]>v[2] && v[1]>v[3]){
+            pixelToBoardNT = drive.actionBuilder(beginPose)
+                    .strafeTo(new Vector2d(-49,32))
+                    .strafeTo(new Vector2d(-49,36))
+                    .strafeToLinearHeading(new Vector2d(48,36),0)
+                    .build();
+        }
+        else if(v[2]>v[1]&& v[2]>v[3]){
+            pixelToBoardNT = drive.actionBuilder(beginPose)
+                    .strafeTo(new Vector2d(-38,32))
+                    .strafeTo(new Vector2d(-38,36))
+                    .strafeToLinearHeading(new Vector2d(48,36),0)
+                    .build();
+        }
+        else{
+            pixelToBoardNT = drive.actionBuilder(beginPose)
+                    .splineTo(new Vector2d(-28, 36), -Math.PI/4)
+                    .strafeToLinearHeading(new Vector2d(-41, 58), 0)
+                    .strafeTo(new Vector2d(30,58))
+//                    .splineTo(new Vector2d(52, 42), 0)
+                    .strafeToLinearHeading(new Vector2d(48,36),0)
+                    .build();
+        }
+
+
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+
+            telemetry.addData("Dreapta",v[1] );
+            telemetry.addData("Stanga",v[2] );
+            telemetry.addData("Mijloc",v[3] );
+            if(nou==OpenCvPipRosu.detectie.Dreapta)telemetry.addLine("Dreapta");
+            else if(nou== OpenCvPipRosu.detectie.Stanga)telemetry.addLine("Stanga");
+            else telemetry.addLine("Mijloc");
+            telemetry.update();
+
+
+            Actions.runBlocking(
+                    new SequentialAction(
+                            pixelToBoardNT
+
+
+//                            drive.actionBuilder( new Pose2d(48, 46, 0)).strafeTo(new Vector2d(53,36)),
+//                            new ParallelAction(
+//                                lift.goTarget(2000),
+//                                    (telemetryPacket) -> {
+//
+//                                        return false;
+//                                    })
+
+
+                    ));
+        }
+//        Actions.runBlocking(drive.actionBuilder( new Pose2d(48, 46, 0))
+//                .strafeTo(new Vector2d(53,36))
+//
+//        );
+//        controlHubCam.stopStreaming();
+        switch (nou){
+
+            case Dreapta:
+
+                break;
+            case Stanga:
+
+                break;
+            case Mijloc:
+
+                break;
+
+        }
+
 
     }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private void initOpenCV(Telemetry telemetry) {
+
+        // Create an instance of the camera
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+
+
+        // Use OpenCvCameraFactory class from FTC SDK to create camera instance
+        controlHubCam = OpenCvCameraFactory.getInstance().createWebcam(
+                hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+
+
+        OpenCvPipRosu openCvPipRosu = new OpenCvPipRosu(telemetry);
+
+        controlHubCam.setPipeline(openCvPipRosu);
+//        controlHubCam.openCameraDevice();
+
+        controlHubCam.startStreaming(CAMERA_WIDTH, CAMERA_HEIGHT, OpenCvCameraRotation.UPRIGHT);
+    }
 }
